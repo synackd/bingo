16b1ed7de53cf96ff777eeee531dddbefaa446e0
Devon Bautista <devontbautista@gmail.com>
2019-02-09 14:22:07 -0700

    Successful test of global lib makefile

---
+++ lib/include/global.hpp
@@ -0,0 +1,6 @@
+#ifndef _GLOBAL_HPP_
+#define _GLOBAL_HPP_
+
+void print(const char*);
+
+#endif
---
+++ lib/src/global.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "global.hpp"
+
+void print(const char *str)
+{
+    printf(str);
+}
