8f349fcd69e40df016b46b6b2eb7660d307d2534
Devon Bautista <devontbautista@gmail.com>
2019-02-12 23:10:53 -0700

    Create virtual base class Socket

---
+++ lib/include/socket.hpp
@@ -0,0 +1,34 @@
+/**
+ * @file socket.hpp
+ */
+
+#ifndef _SOCKET_HPP_
+#define _SOCKET_HPP_
+
+#include <sys/socket.h> /* for socket() and bind() */
+#include <stdio.h>      /* printf() and fprintf() */
+#include <stdlib.h>     /* for atoi() and exit() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * An abstract class representing a general-
+ * purpose socket.
+ */
+class Socket
+{
+    public:
+        getSocket(void);
+        getConnection(void);
+
+    private:
+        int sockfd;     /**< File descriptor of socket */
+        int connfd;     /**< File descriptor of connection */
+}
+
+#endif
