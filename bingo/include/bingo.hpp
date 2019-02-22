4adb9bb626c3f467f52c883b0829661425ba0085
Devon Bautista <devontbautista@gmail.com>
2019-02-21 18:54:39 -0700

    Add global data for bingo program

--- bingo/include/bingo.hpp
+++ bingo/include/bingo.hpp
@@ -8,9 +8,19 @@
 #ifndef _BINGO_HPP_
 #define _BINGO_HPP_
 
+#include "constants.hpp"
 #include "client.hpp"
 
 using namespace std;
+
+/*
+ * Data
+ */
+char mgr_ip[BUFMAX];    /**< Holds the manager's IP address */
+
+vector<Player> players; /**< List of in-game players */
+Game myGame;            /**< This peer's game */
+Player me;              /**< Holds this peer's player data */
 
 /*
  * Functions
